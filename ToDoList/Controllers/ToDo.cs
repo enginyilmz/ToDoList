ae0a316140e3b6678e87889161daf4b3509ca95b
enginyilmz <enginyilmaz.math@hotmail.com>
2017-08-15 11:54:03 +0300

    ToDoList project was completed.

---
+++ ToDoList/Controllers/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ToDoList.Controllers
+{
+    public class ToDo
+    {
+        public ToDo()
+        {
+            CreateDate = DateTime.Now;
+            Guid = Guid.NewGuid();
+        }
+
+        public Guid Guid { get; set; }
+        public string Text { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public static IList<ToDo> ToDos = new List<ToDo>();
+
+        // GET: ToDo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult List()
+        {
+            return JsonSuccess(ToDos);
+        }
+
+        public JsonResult Add(string text)
+        {
+            var newTodo = new ToDo() {Text = text};
+            ToDos.Add(newTodo);
+            return JsonSuccess(newTodo, "The new task has been successfully added.");
+        }
+
+        public JsonResult Update(Guid? Id, string Text)
+        {
+            var todo = ToDos.SingleOrDefault(_ => _.Guid == Id);
+            if (todo != null)
+            {
+                todo.Text = Text;
+                todo.ModifiedDate=DateTime.Now;
+            }
+            else
+            {
+                return JsonError("No Records Found!");
+            }
+
+            return JsonSuccess(todo, "Task is updated.");
+        }
+
+        public JsonResult Delete(Guid? Id)
+        {
+            var todo = ToDos.SingleOrDefault(_ => _.Guid == Id);
+            if (todo != null)
+            {
+                ToDos.Remove(todo);
+            }
+            else
+            {
+                return JsonError("No Records Found!");
+            }
+
+            return JsonSuccess(todo, "Task is deleted.");
+        }
+
+        public JsonResult JsonSuccess(dynamic data, string message="")
+        {
+            return Json(new
+            {
+                IsSuccess=true,
+                Message=message,
+                Data=data
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult JsonError(string message)
+        {
+            return Json(new
+            {
+                IsSuccess = false,
+                Message = message
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
